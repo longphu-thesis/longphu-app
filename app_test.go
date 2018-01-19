2fc26f7ade1c571aa7878bc75c70877bc283b681
Long Phu <PhuHL@sendo.vn>
2018-01-19 17:36:27 +0700

    Update

---
+++ app_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("test")
+
+	// go vet ./...
+	str := "hello world!"
+	//fmt.Printf("%d\n", str)
+	//fmt.Printf("%s\n", &str)
+	fmt.Printf("%s\n", str)
+}
---
+++ example/string_concat/string_concat.go
@@ -0,0 +1,16 @@
+package example
+
+func ConcatOperator(original *string, concat string) {
+	// This could be written as 'return *original + concat'
+	// but I wanted to confirm no special compiler optimizations
+	// existed for concatenating a string to itself.
+	*original = *original + concat
+}
+
+func SelfConcatOperator(input string, n int) string {
+	output := ""
+	for i := 0; i < n; i++ {
+		ConcatOperator(&output, input)
+	}
+	return output
+}
