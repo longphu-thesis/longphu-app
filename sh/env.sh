130fc5f8b0999d117f3aa2c4517cf302930fc8e9
Long Phu <PhuHL@sendo.vn>
2018-01-19 13:24:57 +0700

    Update Travis

--- sh/env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export GO_FILES=$(find . -iname '*.go' -type f | grep -v /vendor/) # All the .go files, excluding vendor/
-export GO_FILES_TEST=$(go list ./... | grep -v /vendor/) # All the .go files, excluding vendor/
--- sh/govendor.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-govendor list
-govendor sync
